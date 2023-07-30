c7ab680787e59d8ba9dae2811fb40a6f168d7ac6
jackybian <happy198308@126.com>
2023-07-30 17:41:41 +0800

    update

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useRef, FC,ReactElement } from "react";
+import { ITodo } from "../typings";
 
-const TdInput = ()=>{
+interface IProps{
+    addTodo: (todo: ITodo) => void
+    todoList:ITodo[];
+}
+
+const TdInput: FC<IProps> = ({
+    addTodo,
+    todoList
+}):ReactElement=>{
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const addItem =():void=>{
+        const val:string = inputRef.current!.value.trim();
+        if (val.length) {
+            const isExist = todoList.find(todo=>todo.content === val);
+
+            if (isExist) {
+                alert('已存在该项');
+                return;
+            }
+
+            addTodo({
+                id: new Date().getTime(),
+                content: val,
+                completed: false
+            });
+
+            inputRef.current!.value = '';
+        }
+    }
+
     return (
-        <div></div>
+        <div>
+            <input type="text" placeholder="plz input" ref={inputRef}></input>
+            <button onClick={addItem}>ADD</button>
+        </div>
     );
 }
 
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { FC, useCallback, useState,ReactElement, useEffect } from "react";
 import TdInput from "./Input";
 import TdList from "./List";
-const TodoList = ()=>{
+import { ITodo } from "./typings";
+const TodoList :FC= (): ReactElement=>{
+    const [todoList, setTodoList] = useState<ITodo[]>([]);
+
+    useEffect(()=>{
+        console.log(todoList);
+    },[todoList])
+
+    const addTodo = useCallback((todo: ITodo)=>{
+        console.log(todo);
+        setTodoList(todoList =>[...todoList, todo])
+    },[])
+
     return (
         <div>
-            <TdInput />
+            <TdInput 
+                addTodo={addTodo}
+                todoList = {todoList}
+            />
             <TdList />
         </div>
     );
